e7cd34a59304b0d77f184c3646fe135e8c658842
wowwowster <wowwowster@gmail.com>
2023-03-21 13:37:49 +0100

    Complétion de la classe DemoEuclideInitial

--- src/main/java/algorithmes/euclide/DemoEuclideInitial.java
+++ src/main/java/algorithmes/euclide/DemoEuclideInitial.java
@@ -2,6 +2,14 @@ package algorithmes.euclide;
 
 public class DemoEuclideInitial {
 
+
+//    Réaliser un programme qui calcule le PGCD de deux nombres à l'aide de l’algorithme d’Euclide.
+//            "On considère que pgcd(a,0)= a et que pour b différent de 0, pgcd(a,b) = (pgcd b, a mod b)".
+//    On divise a par b ce qui donne un reste r < b.
+//    On cherche à ce que le reste de la division soit 0.
+//    Si r = 0 , PGCD (a,b) = b.
+//            Sinon, PGCD (a,b) = PGCD (b,r) car ce qui est commun à a et b le sera pour r.
+
     public static void main (String[] args) {
         System.out.println(pgcd(255,141));
         System.out.println(pgcd(141,255));
@@ -14,15 +22,6 @@ public class DemoEuclideInitial {
     }
 
     public static int pgcd( int first, int second ) {
-
-        while (second!=0) {
-            int reste= first % second;
-            first=second;
-            second=reste;
-            pgcd( first, second );
-        }
-
-            return first;
 
     }
 }
