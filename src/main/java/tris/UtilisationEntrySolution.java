fb75d5e54f70e44a7eb1c021b01641af8b3fa9cb
wowwowster <wowwowster@gmail.com>
2023-03-25 20:54:55 +0100

    Ajout de la classe UtilisationEntrySolution

---
+++ src/main/java/tris/UtilisationEntrySolution.java
@@ -0,0 +1,24 @@
+package tris;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UtilisationEntrySolution
+{
+    public static void main(String[] args)
+    {
+        final Map<String, String> map = new HashMap<>();
+
+        map.put("rouge", "FF0000");
+        map.put("vert", "00FF00");
+        map.put("bleu", "0000FF");
+        for (final Map.Entry<String, String> entry : map.entrySet())
+        {
+            final String key = entry.getKey();
+            final String value = entry.getValue();
+
+            // Traitement key-value
+            System.out.println("key: " + key + " value: " + value);
+        }
+    }
+}
