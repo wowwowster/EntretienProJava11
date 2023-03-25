93b94e244895d078650b137b56c8f83febd0037a
wowwowster <wowwowster@gmail.com>
2023-03-25 20:44:47 +0100

    Ajout d'une classe

---
+++ src/main/java/tris/UtilisationEntryInitial.java
@@ -0,0 +1,17 @@
+package tris;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UtilisationEntryInitial
+{
+    public static void main(String[] args)
+    {
+        final Map<String, String> map = new HashMap<>();
+
+        map.put("rouge", "FF0000");
+        map.put("vert", "00FF00");
+        map.put("bleu", "0000FF");
+    }
+
+}
